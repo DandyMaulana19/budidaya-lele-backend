7878f752a18991d47344365d020f5eb63ee69082
DandyMaulana19 <dandyainul19@gmail.com>
2026-01-05 17:49:24 +0700

    feat: add get total data general report and by pool

--- src/controllers/app.controller.js
+++ src/controllers/app.controller.js
@@ -1,6 +1,12 @@
 import { successResponse } from "../utils/response.js";
-import { activityLogs } from "../database/schema/index.js";
-import { desc } from "drizzle-orm";
+import {
+  activityLogs,
+  feedReports,
+  harvestReports,
+  mortalityReports,
+  seedReports,
+} from "../database/schema/index.js";
+import { count, desc, eq } from "drizzle-orm";
 
 export const getActivityLogs = async (request, reply) => {
   const db = request.server?.db;
@@ -12,4 +18,67 @@ export const getActivityLogs = async (request, reply) => {
     .limit(3);
 
   return successResponse(reply, "data fetched", data, 200);
+};
+
+export const getTotalReports = async (request, reply) => {
+  const db = request.server?.db;
+
+  const totalFeedReports = await db
+    .select({ count: count() })
+    .from(feedReports);
+  const totalHarvestReports = await db
+    .select({ count: count() })
+    .from(harvestReports);
+  const totalMortalityReports = await db
+    .select({ count: count() })
+    .from(mortalityReports);
+  const totalSeedReports = await db
+    .select({ count: count() })
+    .from(seedReports);
+
+  return successResponse(
+    reply,
+    "data fetched",
+    {
+      totalFeedReports: totalFeedReports[0]["count"],
+      totalHarvestReports: totalHarvestReports[0]["count"],
+      totalMortalityReports: totalMortalityReports[0]["count"],
+      totalSeedReports: totalSeedReports[0]["count"],
+    },
+    200
+  );
+};
+
+export const getTotalReportsbyPool = async (request, reply) => {
+  const db = request.server?.db;
+  const poolId = request.params.id;
+
+  const totalFeedReports = await db
+    .select({ count: count() })
+    .from(feedReports)
+    .where(eq(feedReports.poolId, poolId));
+  const totalHarvestReports = await db
+    .select({ count: count() })
+    .from(harvestReports)
+    .where(eq(harvestReports.poolId, poolId));
+  const totalMortalityReports = await db
+    .select({ count: count() })
+    .from(mortalityReports)
+    .where(eq(mortalityReports.poolId, poolId));
+  const totalSeedReports = await db
+    .select({ count: count() })
+    .from(seedReports)
+    .where(eq(seedReports.poolId, poolId));
+
+  return successResponse(
+    reply,
+    "data fetched",
+    {
+      totalFeedReports: totalFeedReports[0]["count"],
+      totalHarvestReports: totalHarvestReports[0]["count"],
+      totalMortalityReports: totalMortalityReports[0]["count"],
+      totalSeedReports: totalSeedReports[0]["count"],
+    },
+    200
+  );
 };
